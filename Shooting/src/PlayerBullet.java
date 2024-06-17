2f637081490f9116b864de8bbdc694d4bcf3e819
sad <ads>
2024-06-17 11:55:16 +0900

    10-1

--- Shooting/src/PlayerBullet.java
+++ Shooting/src/PlayerBullet.java
@@ -1,6 +1,13 @@
 
 public class PlayerBullet extends Character {
+	public void  draw (MyFrame PlayerBullet) {
+		PlayerBullet.setColor(0,128,0);
+		PlayerBullet.fillRect(x+10, y, 10, 10);
+	
+	}
 public PlayerBullet (double x,double y,double vx, double vy ) {
 	super(x,y,vx,vy);
+	
 }
+
 }
