a563325d0888e2a02e3ba6415dcac6cb381767ca
sad <ads>
2024-06-24 10:04:58 +0900

    12-1

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -1,7 +1,9 @@
 import java.util.Vector;
 
-public class GameFrame extends MyFrame {
-public void run() {
+public class GameFrame extends MyFrame 
+{
+public void run() 
+{
 	
 	GameWorld.player=new Player(100,300,0,0);
 	addKeyListener(GameWorld.player);
@@ -10,7 +12,8 @@ public void run() {
 	GameWorld.enemies =new Vector<Enemy>();
 	GameWorld.enemies.add(new EnemyBase(100,50,1,0));
 	
-	while(true) {
+	while(true)
+	{
 		clear();
 	GameWorld.player.draw(this);
 	GameWorld.player.move();
@@ -18,9 +21,27 @@ public void run() {
 	
     moveEnemies();
 	
+CheckPlayerAndEnemies();
+    
 	sleep(0.03);
 	}
 }
+
+public void CheckPlayerAndEnemies()
+{
+    for(int i=0; i<GameWorld.enemies.size(); i++) 
+    {
+    Enemy e=GameWorld.enemies.get(i);
+    if	(Math.abs(e.x-GameWorld.player.x)<=30 && 
+    	 Math.abs(e.y-GameWorld.player.y)<=30) 
+    {
+    	System.out.println("やられた");
+    	GameWorld.player.y=-1000;
+    }
+    }
+    
+}
+
 	public void moveEnemies() {
 		for(int i=0; i<GameWorld.enemies.size();i++) {
 			Enemy e=GameWorld.enemies.get(i);
